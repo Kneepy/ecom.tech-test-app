1b584d68bc063edd0cc95dc5c7aa7d36d0af36f0
Kneepy <kneepydev@gmail.com>
2026-03-14 12:13:44 +0800

    add workflows

---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from "node:path";
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@components': path.resolve(__dirname, './src/components'),
+            '@features': path.resolve(__dirname, './src/features'),
+            '@styles': path.resolve(__dirname, './src/styles'),
+            '@store': path.resolve(__dirname, './src/store'),
+            '@routes': path.resolve(__dirname, './src/routes'),
+            '@pages': path.resolve(__dirname, './src/pages'),
+            '@layouts': path.resolve(__dirname, './src/layouts'),
+            '@api': path.resolve(__dirname, './src/api'),
+            '@assets': path.resolve(__dirname, './src/assets'),
+        }
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@use "@/styles/variables" as *;`
+            },
+        },
+    },
+});
