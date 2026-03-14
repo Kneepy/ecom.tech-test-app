ccbc9a97033d19a2c58fec666ea296354d98acb1
Kneepy <kneepydev@gmail.com>
2026-03-14 20:07:16 +0800

    Надеюсь the end

--- src/api/product.ts
+++ src/api/product.ts
@@ -3,7 +3,7 @@ import testImage from "@assets/images/test.png"
 
 export const MockProductApi = {
     async getAll(): Promise<Product[]> {
-        return new Promise((resolve) => resolve(Array(20).fill(1).map((_, i) => ({
+        return new Promise((resolve) => resolve(Array(10).fill(1).map((_, i) => ({
             id: i,
             title: `Product ${i} Звук клавиш печатной машинки развеял последние сомнения`,
             price: Math.round(1000*Math.random()),
